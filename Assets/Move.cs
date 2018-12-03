363d97b44e7dd6766d086c6d6d78f438ae7aed8d
Xia <ziyuxia@ads.iu.edu>
2018-12-03 09:25:21 -0500

    fix probuilder

---
+++ Assets/Move.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+    Rigidbody rg;
+	// Use this for initialization
+	void Start () {
+        rg = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+
+        }
+	}
+}
